a764fb1d28e0993010ccf4ee8bde42db4c541c7d
Ana <ana.lau.villarreal@gmail.com>
2019-08-29 16:06:33 -0300

    mas pequeños cambios

--- resources/views/goals/listadoCarreras.blade.php
+++ resources/views/goals/listadoCarreras.blade.php
@@ -4,32 +4,17 @@
     <h1>Proximos desafíos</h1>
     <br>
     <div class="container">
-      <div class="carrete">
-      <section class="col-xl-6 col-md-6 col-sm-6">
+      <div class="row">
+      <div class="col-6">
+      <img id=foto class="foto" src="img/fotosFlyers/flyer carrera1.jpg"/>
+      <img id=foto class="foto" src="img/fotosFlyers/flyercarrera2.jpg"/>
       </div>
-       <img class="foto" src="img/fotosFlyers/flyer carrera1.jpg" width="500px">
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera2.jpg" width="500px"/>
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera3.jpg" width="500px"/>
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera4.jpg" width="500px"/>
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera5.jpg" width="500px"/>
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera6.jpg" width="500px"/>
-       <br>
-       <br>
-       <img class="foto" src="img/fotosFlyers/flyercarrera7.jpg" width="500px"/>
-       <br>
-       </div>
-     </section>
-  </div>
+      <div class="col-6">
+      <img id=foto class="foto" src="img/fotosFlyers/flyercarrera3.jpg"/>
+      <img id=foto class="foto" src="img/fotosFlyers/flyercarrera4.jpg"/>
+      </div>
+     </div>
+     </div>
   <script src="js/script.js"></script>
 </main>
 @endsection
